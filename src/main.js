befc6cd53697023b893be0c95bcad20fc4c05ffe
Bogdan Plieshka <bplieshka@ebay.com>
2015-07-17 15:08:34 +0300

    #2: [Feature] Expand functionality
    - decomposed plugin to methods

--- src/main.js
+++ src/main.js
@@ -3,51 +3,78 @@
     'use strict';
 
     var name = 'sideScroll',
-        defaults = {
+        win = $(window),
+        winHeight = 0,
+        winTop = 0,
+        winTopLast = 0,
+        nextTop = 0,
+        contentHeight = 0,
+        sidebarHeightRest = 0,
+        scrollStep = 0;
+
+    function SideScroll(el, options) {
+
+        this.options = $.extend({}, {
             contentClass: '.content',
             fixedClassName: 'is-fixed'
-        };
+        }, options);
 
-    function SideScroll(element, options) {
-        this.options = $.extend({}, defaults, options);
-        this.init();
+        var sidebar = $(el);
 
-        var win = $(window),
-            content = $(this.options.contentClass),
-            winHeight = 0,
-            sidebar = $(element),
-            sidebarHeight = sidebar.height(),
-            sidebarHeightRest = 0,
-            contentHeight,
-            winTop = 0,
-            winTopLast = 0,
-            scrollStep = 0,
-            nextTop = 0,
-            fixedClass = this.options.fixedClassName;
+        this._init(sidebar);
+        this._bind(sidebar);
+    }
 
-        function isSidebarFixed() {
-            return (sidebarHeight < contentHeight) && (contentHeight > winHeight);
-        }
+    SideScroll.prototype = {
+        _init: function (el) {
+            this._positionSidebar(el);
+        },
+        _bind: function (el) {
+            var self = this,
+                sidebar = $(el),
+                sidebarHeight = sidebar.height();
 
-        function addFixed(el) {
-            if (!el.hasClass(fixedClass)) {
-                el.addClass(fixedClass);
-            }
-        }
+            //  Change position on scroll
+            win.on('scroll', function () {
+                self._positionSidebar(el);
+            });
 
-        function removeFixed(el) {
-            if (el.hasClass(fixedClass)) {
-                el.removeClass(fixedClass);
-            }
-        }
+            win.on('resize', function () {
+                winHeight = win.height();
+                //  Reset position if fixed and out of smart scroll
+                if ((sidebarHeight < contentHeight) && (sidebarHeight <= winHeight)) {
+                    sidebar.removeAttr('style');
+                }
+            });
 
-        function positionSidebar(el) {
+        },
+        _positionSidebar: function (el) {
+            var content = $(this.options.contentClass),
+                sidebarHeight = el.height(),
+                fixedClass = this.options.fixedClassName;
+
             //  Spot positions and heights
             winHeight = win.height();
             contentHeight = content.height();
             winTop = win.scrollTop();
             scrollStep = winTop - winTopLast; // scroll step
             sidebarHeightRest = sidebarHeight - winTop; // visible sidebar height
+
+            function isSidebarFixed() {
+                return (sidebarHeight < contentHeight) && (contentHeight > winHeight);
+            }
+
+            function addFixed(el) {
+                if (!el.hasClass(fixedClass)) {
+                    el.addClass(fixedClass);
+                }
+            }
+
+            function removeFixed(el) {
+                if (el.hasClass(fixedClass)) {
+                    el.removeClass(fixedClass);
+                }
+            }
 
             // Fixed sidebar cases
             if (isSidebarFixed()) {
@@ -70,7 +97,8 @@
 
                         el.css('top', -nextTop);
 
-                    } else if (winTop < winTopLast) {//  Scroll up
+                    } else if (winTop < winTopLast) { //  Scroll up
+
                         if (nextTop > -scrollStep) {
                             nextTop += scrollStep;
                         } else {
@@ -83,31 +111,6 @@
                 removeFixed(el);
             }
             winTopLast = winTop; //  Save previous window scrollTop
-        }
-
-        //  Page start calculation
-        positionSidebar(sidebar);
-
-        //  Change position on scroll
-        win.on('scroll', function () {
-            positionSidebar(sidebar);
-        });
-
-        win.on('resize', function () {
-            winHeight = win.height();
-            //  Reset position if fixed and out of smart scroll
-            if ((sidebarHeight < contentHeight) && (sidebarHeight <= winHeight)) {
-                sidebar.removeAttr('style');
-            }
-        });
-    }
-
-    SideScroll.prototype = {
-        init: function () {
-            console.log('hello');
-        },
-        destroy: function () {
-            console.log('destroy');
         }
     };
 
